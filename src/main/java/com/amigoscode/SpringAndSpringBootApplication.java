67cc8e8a5edd7255c361cb081509dfd5b6f1a744
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-02-23 11:59:00 +0530

    New branch for embedded web server. adding new files.

---
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SpringAndSpringBootApplication.class,
+                args
+        );
+        System.out.println("Hello World!");
+    }
+
+}
