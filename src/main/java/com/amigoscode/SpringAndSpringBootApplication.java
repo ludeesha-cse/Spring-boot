0dc53d0385aa49eb702853d17ca4060518608021
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-02-28 16:10:51 +0530

    Add REST controller and simplify application structure

--- src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -1,21 +1,15 @@
 package com.amigoscode;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
+@RestController
 @SpringBootApplication
 public class SpringAndSpringBootApplication {
 
@@ -24,82 +18,24 @@ public class SpringAndSpringBootApplication {
                 SpringAndSpringBootApplication.class,
                 args
         );
-
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-//            System.out.println(beanDefinitionName);
-        }
-        System.out.println(beanDefinitionNames.length);
     }
 
-    @Bean
-    @Primary
-    public String redBean(){
-        return "redBean";
-    }
+    public enum Gender {MALE, FEMALE}
 
-    @Bean
-    public String BlueBean(){
-        return "BlueBean";
-    }
-
-    @Bean
-    CommandLineRunner CommandLineRunner (String redBean, String blueBean, UserService userService) {
-        return args -> {
-            System.out.println("Hello From commandLineRunner 1");
-            System.out.println(redBean);
-            System.out.println(blueBean);
-            System.out.println(userService.getUsers());
-            System.out.println(userService.getUserById(2));
-        };
-    }
-
-    @Bean
-    CommandLineRunner CommandLineRunner2 (String redBean, String blueBean) {
-        return args -> {
-            System.out.println("Hello From commandLineRunner 2");
-            System.out.println(redBean);
-            System.out.println(blueBean);
-        };
-    }
-
-    public record User(int id, String name) {
+    public record Person(String name, int id, int age, Gender gender){
 
     }
 
-//    @Repository - for data
-//    @Service - for business logic
-//    @Component - for general purpose
-    @Service
-    public class UserService {
+    public static List<Person> People = new ArrayList<>();
 
-        public UserService() {
-            System.out.println("UserService Constructor");
-        }
+    static {
+        People.add(new Person("James", 1, 23, Gender.MALE));
+        People.add(new Person("Maria", 2, 20, Gender.FEMALE));
+        People.add(new Person("John", 3, 27, Gender.MALE));
+    }
 
-        @PostConstruct
-        public void init(){
-            System.out.println("fill cache");
-        }
-
-        @PreDestroy
-        public void destroy(){
-            System.out.println("clear cache");
-        }
-        public List<User> getUsers() {
-            return List.of(
-                    new User(1, "James"),
-                    new User(2, "Maria"),
-                    new User(3, "Anna")
-            );
-        }
-
-        public Optional<User> getUserById(int id) {
-            return getUsers().stream()
-                    .filter(user -> user.id() == id)
-                    .findFirst();
-        }
-
-
+    @GetMapping
+    public List<Person> getPeople() {
+        return People;
     }
 }
