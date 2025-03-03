903e901274e8ecae0c3c21f8b1903bd7784c7ee8
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-03-03 17:55:47 +0530

    Implement CRUD operations for Person entity in REST API

--- src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -1,17 +1,16 @@
 package com.amigoscode;
 
+import jdk.jfr.Frequency;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @RestController
@@ -29,16 +28,20 @@ public class SpringAndSpringBootApplication {
 
     public enum SortOrder {ASC, DESC}
 
-    public record Person(String name, int id, int age, Gender gender){
+    private static final AtomicInteger id = new AtomicInteger(0);
+
+    public record Person(Integer id, String name, Integer age, Gender gender){
 
     }
+
+    public record PersonUpdateRequest(String name, Integer age){}
 
     public static List<Person> People = new ArrayList<>();
 
     static {
-        People.add(new Person("James", 1, 23, Gender.MALE));
-        People.add(new Person("Maria", 2, 20, Gender.FEMALE));
-        People.add(new Person("John", 3, 27, Gender.MALE));
+        People.add(new Person(id.incrementAndGet(), "James", 23, Gender.MALE));
+        People.add(new Person( id.incrementAndGet(), "Maria",20, Gender.FEMALE));
+        People.add(new Person(id.incrementAndGet(), "John", 27, Gender.MALE));
     }
 
     @GetMapping
@@ -55,7 +58,39 @@ public class SpringAndSpringBootApplication {
     @GetMapping("{id}")
     public Optional<Person> getPersonById(@PathVariable("id") Integer id) {
         return People.stream()
-                .filter(person -> person.id() == id)
+                .filter(person -> person.id().equals(id))
                 .findFirst();
+    }
+
+    @DeleteMapping("{id}")
+    public void deletePersonById(@PathVariable("id") Integer id) {
+        People.removeIf(person -> person.id().equals(id));
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
+        People.add(new Person(
+                id.incrementAndGet(),
+                person.name(),
+                person.age(),
+                person.gender())
+        );
+    }
+
+    @PutMapping("{id}")
+    public void updatePerson(@PathVariable("id") Integer id, @RequestBody PersonUpdateRequest request) {
+        People.stream()
+                .filter(p -> p.id().equals(id))
+                .findFirst()
+                .ifPresent(p -> {
+                    if (request.name() != null && !request.name().isEmpty() && !request.name().equals(p.name())) {
+                        Person person = new Person(p.id, request.name(), p.age(), p.gender());
+                        People.set(People.indexOf(p), person);
+                    }
+                    if (request.age() != null && !request.age().equals(p.age()) ) {
+                        Person person = new Person(p.id(), p.name(), request.age(), p.gender());
+                        People.set(People.indexOf(p), person);
+                    }
+                });
     }
 }
